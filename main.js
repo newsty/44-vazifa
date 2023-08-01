c651af6fd8014f0b50b5ef06e2388fbe53609b72
newsty <xadichaermatova19@gmail.com>
2023-08-01 23:49:12 +0500

    map,forEach,reduce

---
+++ main.js
@@ -0,0 +1,73 @@
+
+// map
+// 1-misol
+//const person=[
+    //     {name:'Zebo',age:26,gender:'women'},
+    //     {name:'Bobur',age:16,gender:'men'},
+    //     {name:'Rustam',age:20,gender:'men'},
+    //     {name:'Shahlo',age:19,gender:'women'},
+    // ]
+// const myMap = person.map((item) => {
+//     return {name:item.name, age:item.age +1, gender:item.gender,yers:2023-item.age}
+// })
+
+// 2-misol
+// const number=[1,2,3,4,5,6,7,8,9]
+// const numMap=number.map((item)=>{
+//     return item**2
+// })
+
+
+
+
+
+
+// filter
+// 1-misol
+// const number=[5,4,9,-8,45,-9]
+// const myFilter=number.filter(function(item) {
+//     return item >=0
+// })
+
+// 2-misol
+// const students=[
+//     {name:'Namuna',age:19,gender:'women',ball:5},
+//     {name:'Zebo',age:18,gender:'women',ball:4},
+//     {name:'Zohid',age:21,gender:'men',ball:4},
+//     {name:'Vali',age:20,gender:'men',ball:4},
+// ]
+// const a=5
+// const newFilter=students.filter((item)=>{
+//     return a===item.ball
+// })
+
+
+
+
+
+// forEach
+
+// 1-misol
+// const myName=['lola','robiya','zebo','donyor','zuxra']
+// myName.forEach((item)=>{
+//  const newName = item.charAt().toUpperCase()+item.slice(1)
+//  console.log(newName);
+// });
+
+// 2-misol
+// const number=[45,48,89,25,69,63]
+// number.forEach((item)=>{
+//     const newNumber=item-10
+//     console.log(newNumber);
+// })
+
+
+
+// reduce
+// 1-misol
+// const sum=[200,100,300,450,214,564]
+// const total=sum.reduce((i,item)=>i+item)
+
+// 2-misol
+// const myReduce=['assalomu','  ','alaykum']
+// const myText=myReduce.reduce((a,b)=>a+b)
